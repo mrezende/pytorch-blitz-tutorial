aa7b8462797c0cb13b9ed5f9e574013f196b916d
Marcelo Martins <rezende.martins@gmail.com>
2018-04-08 17:48:20 -0300

    cuda tensors

--- introduction/tensors.py
+++ introduction/tensors.py
@@ -2,9 +2,10 @@ from __future__ import print_function
 import torch
 import numpy as np
 
-a = np.ones(5)
-b = torch.from_numpy(a)
-np.add(a, 1, out=a)
-print(a)
-print(b)
+x = torch.randn(5,3)
+y = torch.randn(5,3)
+if torch.cuda.is_available():
+    x = x.cuda()
+    y = y.cuda()
+    print(x + y)
 

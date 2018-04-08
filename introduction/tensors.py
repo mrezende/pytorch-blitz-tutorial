930fad3ea280feba3a8eead7560878630ab87f90
Marcelo Martins <rezende.martins@gmail.com>
2018-04-08 17:45:22 -0300

    converting tensor to numpy

--- introduction/tensors.py
+++ introduction/tensors.py
@@ -1,14 +1,10 @@
 from __future__ import print_function
 import torch
+import numpy as np
 
-a = torch.ones(5)
-print(a)
-
-b = a.numpy()
-print(b)
-
-a.add_(1)
-
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add_(a, 1, out=a)
 print(a)
 print(b)
 

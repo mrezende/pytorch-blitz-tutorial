1cd82ad2d7acd30a508716d592d631f0229bd63b
Marcelo Martins <rezende.martins@gmail.com>
2018-04-08 17:40:17 -0300

    first commit

---
+++ introduction/tensors.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import torch
+
+x = torch.randn(4,4)
+y = x.view(16)
+z = x.view(-1, 8)
+
+print(x.size(), y.size(), z.size())
+
+print(x)
+print(y)
+print(z)
+
